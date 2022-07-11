deade2f461c2722abbcd9c441435a80bec07b037
Develon2015 <302615249@qq.com>
2022-07-11 22:27:20 +0800

    启动脚本

---
+++ 模拟器.bat
@@ -0,0 +1,1 @@
+"C:\Program Files\TxGameAssistant\UI\AndroidEmulatorEx.exe"  -cmd StartApk -param  -googlepkgname "com.android.vending" -startpkg com.android.vending  -engine "aow" -vm "100" -from AppMarket
